adf1f445b6fc190ffb495210d25b679bf2a59ad0
Dalia Nabil <dalianabil216@gmail.com>
2026-06-14 03:19:59 +0300

    Remove dist folder from repository

--- dist/Modules/Auth/auth.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class AuthService {
-    health = (body) => {
-        return body;
-    };
-}
-exports.default = new AuthService();
